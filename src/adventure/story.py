3ed289b2be22622df369592f8dbf27a216c82019
S-mander <184024871+S-mander@users.noreply.github.com>
2025-10-30 22:38:01 -0400

    Improved story.py formatting with rich library (#2)
    * Improved story.py formatting with rich library
    
    * Made direction choice text appear in bold

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,19 @@
 from adventure.utils import read_events_from_file
 import random
+from rich.console import Console
+from rich.text import Text
+from rich.prompt import Prompt
+
+#list of rich colors for random function
+COLORS = ["red", "green", "blue", "yellow", "magenta", "cyan", "white", "bright_red", 
+         "bright_green", "bright_blue", "bright_yellow", "bright_magenta", "bright_cyan"]
+
+console = Console()
+
+def get_random_colored_text(text: str) -> Text:
+    """returning the text with a random color to make things more interesting"""
+    color = random.choice(COLORS)
+    return Text(text, style=color)
 
 default_message = "You stand still, unsure what to do. The forest swallows you."
 
@@ -11,22 +25,22 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return default_message
+        return f"You stand still, unsure what to do. The forest swallows you."
 
 def left_path(event):
-    return "You walk left. " + event
+    return f"You walk left. {event}"
 
 def right_path(event):
-    return "You walk right. " + event
+    return f"You walk right. {event}"
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    console.print("You wake up in a dark forest. You can go left or right.", style="bold magenta underline")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
+        choice = Prompt.ask("[bold][cyan]Which direction do you choose? (left/right/exit): [/cyan][/bold]")
         choice = choice.strip().lower()
         if choice == 'exit':
             break
         
-        print(step(choice, events))
+        console.print(get_random_colored_text(step(choice, events)))
